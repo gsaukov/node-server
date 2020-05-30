4cfcf275eb721d020ade6d076bf188728ee42358
georgy.saukov <georgy.saukov@wirecard.com>
2020-05-30 23:18:34 +0200

    node server categories/positions

--- controllers/category.js
+++ controllers/category.js
@@ -1,24 +1,57 @@
-module.exports.getAll = function (req, res) {
-    res.status(200).json({
-        categories: 'category'})
+const Category = require('../models/category')
+const Position = require('../models/position')
+const errorHandler = require('../utils/errorHandler')
+
+module.exports.getAll = async function (req, res) {
+    try{
+        const categories = await Category.find({user: req.user.id})
+        res.status(200).json(categories)
+    }catch (e) {
+        errorHandler(res, e)
+    }
 }
 
-module.exports.getById = function (req, res) {
-    res.status(200).json({
-        overview: true})
+module.exports.getById = async function (req, res) {
+    try{
+        const category = await Category.findById(req.params.id)
+        res.status(200).json(category)
+    }catch (e) {
+        errorHandler(res, e)
+    }
 }
 
-module.exports.remove = function (req, res) {
-    res.status(200).json({
-        overview: true})
+module.exports.remove = async function (req, res) {
+    try{
+        await Category.remove({_id: req.params.id})
+        await Position.remove({category: req.params.id})
+        res.status(200).json('category and related positions removed')
+    }catch (e) {
+        errorHandler(res, e)
+    }
 }
 
-module.exports.create = function (req, res) {
-    res.status(200).json({
-        overview: true})
+module.exports.create = async function (req, res) {
+    try{
+        const category = await new Category({
+            name: req.body.name,
+            imageSrc: req.body.imageSrc,
+            user: req.user.id //comes from passport
+        }).save()
+        res.status(200).json(category)
+    }catch (e) {
+        errorHandler(res, e)
+    }
 }
 
-module.exports.update = function (req, res) {
-    res.status(200).json({
-        overview: true})
+module.exports.update = async function (req, res) {
+    try{
+        const category = await Category.findOneAndUpdate(
+            {_id: req.params.id},
+            {$set: req.body},
+            {new: true}
+        )
+        res.status(200).json(category)
+    }catch (e) {
+        errorHandler(res, e)
+    }
 }
--- controllers/position.js
+++ controllers/position.js
@@ -1,19 +1,50 @@
-module.exports.getBycategoryId = function (req, res) {
-    res.status(200).json({
-        overview: true})
+const Position = require('../models/position')
+const errorHandler = require('../utils/errorHandler')
+
+module.exports.getBycategoryId = async function (req, res) {
+    try{
+        const positions = await Position.find({
+            category: req.params.categoryId, //url param
+            user: req.user.id //comes from passport
+        })
+        res.status(200).json(positions)
+    }catch (e) {
+        errorHandler(res, e)
+    }
 }
 
-module.exports.create = function (req, res) {
-    res.status(200).json({
-        overview: true})
+module.exports.create = async function (req, res) {
+    try{
+        const position = await new Position({
+            name: req.body.name,
+            cost: req.body.cost,
+            category: req.body.category,
+            user: req.user.id //comes from passport
+        }).save()
+        res.status(200).json(position)
+    }catch (e) {
+        errorHandler(res, e)
+    }
 }
 
-module.exports.remove = function (req, res) {
-    res.status(200).json({
-        overview: true})
+module.exports.remove = async function (req, res) {
+    try{
+        await Position.remove({_id: req.params.id})
+        res.status(200).json('position removed')
+    }catch (e) {
+        errorHandler(res, e)
+    }
 }
 
-module.exports.update = function (req, res) {
-    res.status(200).json({
-        overview: true})
+module.exports.update = async function (req, res) {
+    try{
+        const position = await Position.findOneAndUpdate(
+            {_id: req.params.id},
+            {$set: req.body},
+            {new: true}
+        )
+        res.status(200).json(position)
+    }catch (e) {
+        errorHandler(res, e)
+    }
 }
