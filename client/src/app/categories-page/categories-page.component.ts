a252ec1621a921370094591532d5ccde75532589
georgy.saukov <georgy.saukov@wirecard.com>
2020-06-07 01:08:05 +0200

    angular client category page and loader.

--- client/src/app/categories-page/categories-page.component.ts
+++ client/src/app/categories-page/categories-page.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {CategoriesService} from "../shared/services/categories.service";
+import {Category} from "../shared/services/interfaces";
 
 @Component({
   selector: 'app-categories-page',
@@ -6,9 +8,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoriesPageComponent implements OnInit {
 
-  constructor() { }
+  loading = false;
+  categories: Category[] = []
+
+  constructor(private categoriesService: CategoriesService) { }
 
   ngOnInit(): void {
+    this.loading = true
+    this.categoriesService.fetch().subscribe(categories => {
+      this.categories = categories;
+      this.loading = false
+    })
+
   }
 
 }
