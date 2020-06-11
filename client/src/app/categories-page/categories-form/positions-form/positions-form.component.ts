63c39febf8929e786e6d9429b803c415e1aa6b18
georgy.saukov <georgy.saukov@wirecard.com>
2020-06-12 00:52:24 +0200

    angular client position CRUD operations

--- client/src/app/categories-page/categories-form/positions-form/positions-form.component.ts
+++ client/src/app/categories-page/categories-form/positions-form/positions-form.component.ts
@@ -2,6 +2,7 @@ import {AfterViewInit, Component, ElementRef, Input, OnDestroy, OnInit, ViewChil
 import {PositionsService} from "../../../shared/services/positions.service";
 import {Position} from "../../../shared/services/interfaces";
 import {MaterialInstance, MaterialService} from "../../../shared/classes/material.service";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 
 @Component({
@@ -15,12 +16,17 @@ export class PositionsFormComponent implements OnInit, AfterViewInit, OnDestroy 
   @Input('categryId') categoryId
   @ViewChild('modal') modalRef: ElementRef
   positions: Position[] = []
-  loading = false;
+  positionId = null
+  loading = false
   modal: MaterialInstance
+  form: FormGroup
 
   ngOnInit(): void {
     this.loading = true;
-    // debugger
+    this.form = new FormGroup({
+      name: new FormControl(null, Validators.required),
+      cost: new FormControl(null, [Validators.required, Validators.min(1), Validators.pattern("^[0-9]*$")])
+    })
     this.positionsService.fetch(this.categoryId).subscribe(
       positions => {
         this.positions = positions
@@ -38,18 +44,86 @@ export class PositionsFormComponent implements OnInit, AfterViewInit, OnDestroy 
   }
 
   onSelectedPosition(position: Position) {
+    this.form.patchValue({
+      name: position.name,
+      cost: position.cost
+    })
+    this.positionId = position._id
     this.modal.open()
+    MaterialService.updateTextInputs()
   }
 
   onAddPosition() {
+    this.positionId = null
+    this.form.reset({
+      name: '',
+      cost: 1
+    })
     this.modal.open()
+    MaterialService.updateTextInputs()
   }
 
   onCancel() {
     this.modal.close()
   }
 
-  onSave() {
+  onSubmit() {
+    this.form.disable()
+    const newPosition: Position = {
+      name: this.form.value.name,
+      cost: this.form.value.cost,
+      category: this.categoryId
+    }
+
+    const completed = () => {
+      this.modal.close()
+      this.form.reset({name: '', cost: 1})
+      this.form.enable()
+    }
+
+    if(this.positionId){
+      newPosition._id = this.positionId
+      this.positionsService.update(newPosition).subscribe(
+        (position) => {
+          const idx = this.positions.findIndex(p => p._id === position._id)
+          this.positions[idx] = position
+          MaterialService.toast('Position was updated')
+        },
+        (error) => {
+          this.form.enable()
+          MaterialService.toast(error.error.message)
+        },
+        completed
+      )
+    } else {
+      this.positionsService.create(newPosition).subscribe(
+        (position) => {
+          MaterialService.toast('Position was added')
+          this.positions.push(position)
+        },
+        (error) => {
+          this.form.enable()
+          MaterialService.toast(error.error.message)
+        },
+        completed
+      )
+    }
 
   }
+
+  onDeletePosition(event: Event, position: Position) {
+    event.stopPropagation();
+    const decision = window.confirm(`Delete position ${position.name}?`)
+    this.positionsService.delete(position).subscribe(
+      (message) => {
+        MaterialService.toast('Position was deleted')
+        const idx = this.positions.findIndex(p => p._id === position._id)
+        this.positions.splice(idx,1)
+      },
+      (error) => {
+        MaterialService.toast(error.error.message)
+      },
+    )
+  }
+
 }
