ad6edfab21d97a81562d2045155acc7b99c9480d
georgy.saukov <georgy.saukov@wirecard.com>
2020-08-03 00:58:15 +0200

    angular client order history

--- client/src/app/history-page/history-page.component.ts
+++ client/src/app/history-page/history-page.component.ts
@@ -1,14 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {MaterialInstance, MaterialService} from "../shared/classes/material.service";
 
 @Component({
   selector: 'app-history-page',
   templateUrl: './history-page.component.html'
 })
-export class HistoryPageComponent implements OnInit {
+export class HistoryPageComponent implements OnInit, OnDestroy, AfterViewInit {
+  @ViewChild('tooltip') tooltipRef: ElementRef
+  tooltip: MaterialInstance
+  isFilterVisible = false;
 
   constructor() { }
 
   ngOnInit(): void {
+    MaterialService
+  }
+
+  ngAfterViewInit(): void {
+    this.tooltip = MaterialService.initTooltip(this.tooltipRef)
+  }
+
+  ngOnDestroy(): void {
+    this.tooltip.destroy()
   }
 
 }
