5a50c4844d6afab205c482bff1b459118bbdca35
georgy.saukov <georgy.saukov@wirecard.com>
2020-08-02 23:55:44 +0200

    angular client order history

---
+++ client/src/app/history-page/history-list/history-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-history-list',
+  templateUrl: './history-list.component.html'
+})
+export class HistoryListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
